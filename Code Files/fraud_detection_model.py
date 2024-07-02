9fd8444f2dc175e234c588ecc9f4bc3fbeeeafb2
Himanshu Sharma <himanshusharma24795@gmail.com>
2024-07-02 14:33:25 +0530

    Update fraud_detection_model.py

--- Code Files/fraud_detection_model.py
+++ Code Files/fraud_detection_model.py
@@ -117,6 +117,7 @@ feature_importance_df = feature_importance_df.sort_values(by='importance', ascen
 
 # Plot feature importance
 sns.barplot(x='importance', y='feature', data=feature_importance_df.head(10))
+# 10 Impoertant Feature 
 plt.title('Top 10 Important Features')
 # Model Show in bar
 plt.show()
